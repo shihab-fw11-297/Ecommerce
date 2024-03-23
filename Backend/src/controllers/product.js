67df5cebf7d219b7e9093bccb17d27275d211b50
shihab shaikh <ind8588@gmail.com>
2024-03-23 12:04:53 +0530

    completed backend products apis

--- Backend/src/controllers/product.js
+++ Backend/src/controllers/product.js
@@ -3,6 +3,7 @@ const { TryCatch } = require('../middlewares/error');
 const ErrorHandler = require("../utils/errorHandler");
 const { rm } = require("fs");
 const Product = modelsDB.product;
+const sequelize = modelsDB.sequelize;
 
 
 const newProduct = TryCatch(
@@ -57,8 +58,95 @@ const getAllProducts = TryCatch(async (req, res, next) => {
 
 });
 
-const getAllCategories = TryCatch(async (req, res, next) => {
+
+const getAdminProducts = TryCatch(async (req, res, next) => {
+    let products = await Product.findAll({});
+
+    return res.status(200).json({
+        success: true,
+        products,
+    });
 
 });
 
-module.exports = { newProduct, getlatestProducts, getAllProducts, getAllCategories }
+const getSingleProduct = TryCatch(async (req, res, next) => {
+    const id = req.params.id;
+    let products = await Product.findByPk(id);
+
+    return res.status(200).json({
+        success: true,
+        products,
+    });
+
+});
+
+const deleteProduct = TryCatch(async (req, res, next) => {
+    const id = req.params.id;
+    let products = await Product.findByPk(id);
+
+    if (!products) return next(new ErrorHandler("Product Not Found", 404));
+
+    rm(products.photo, () => {
+        console.log("Product Photo Deleted");
+    });
+
+    await products.destroy({
+        where: {
+            id: id
+        }
+    });
+
+    return res.status(200).json({
+        success: true,
+        message: "Product Deleted Successfully",
+    });
+});
+
+const updateProduct = TryCatch(async (req, res, next) => {
+    const { id } = req.params;
+    const { name, price, stock, category } = req.body;
+    const photo = req.file;
+    const product = await Product.findByPk(id);
+    let updatedFields = {};
+    
+    if (!product) return next(new ErrorHandler("Product Not Found", 404));
+
+    if (photo) {
+        rm(product.photo, () => {
+            console.log("Old Photo Deleted");
+        });
+        updatedFields.photo = photo.path;
+    }
+
+    if (name) updatedFields.name = name;
+    if (price) updatedFields.price = price;
+    if (stock) updatedFields.stock = stock;
+    if (category) updatedFields.category = category;
+  
+     await Product.update(updatedFields, {
+        where: { id: id },
+        returning: true, // Include this option to return the updated record
+      });
+    
+    return res.status(200).json({
+        success: true,
+        message: "Product Updated Successfully"
+    });
+});
+
+const getAllCategories = TryCatch(async (req, res, next) => {
+
+    const categories = await Product.findAll({
+        attributes: [
+            [sequelize.fn('DISTINCT', sequelize.col('category')), 'category']
+        ]
+    });
+    const distinctCategories = categories.map(category => category.category);
+
+    return res.status(200).json({
+        success: true,
+        distinctCategories,
+    });
+});
+
+module.exports = { newProduct, getlatestProducts, getAllProducts, getAllCategories, getAdminProducts, getSingleProduct, deleteProduct, updateProduct }
--- Backend/src/routes/products.js
+++ Backend/src/routes/products.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express.Router();
 const { adminOnly } = require("../middlewares/auth.js");
 const singleUpload = require("../middlewares/multer.js");
-const { newProduct,getlatestProducts,getAllProducts,getAllCategories } = require("../controllers/product.js");
+const { newProduct,getlatestProducts,getAllProducts,getAllCategories,getAdminProducts,getSingleProduct, updateProduct, deleteProduct } = require("../controllers/product.js");
 
 //To Create New Product  - /api/v1/product/new
 app.post("/new", singleUpload, newProduct);
@@ -15,5 +15,10 @@ app.get("/all", getAllProducts);
 
 //To get all unique Categories  - /api/v1/product/categories
 app.get("/categories", getAllCategories);
+
+//To get all Products   - /api/v1/product/admin-products
+app.get("/admin-products", getAdminProducts);
+
+app.route("/:id").get(getSingleProduct).put(singleUpload,updateProduct).delete(deleteProduct);
 
 module.exports = app;
